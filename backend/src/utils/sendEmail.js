493fb7cae3c261389c885cb7f83294978c78c170
Keneth Garita <Kenethgarita@gmail.com>
2025-10-21 15:53:54 -0600

    .

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -1,1 +1,15 @@
 import Nodemailer from "nodemailer"
+
+export const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.email",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: "kenethgarita@gmail.com",
+    pass: "dgyr sttc sfcl nwpn",
+  },
+});
+
+transporter.verify(() => {
+    console.log("Ready to send emails")
+})
