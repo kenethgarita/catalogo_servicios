bcb5476aaf7a9e05209cbf04dae19134423a0878
Fuentes <aletoonx@gmail.com>
2025-10-31 10:30:09 -0600

    AdminCategoria

--- frontend/catalogo/src/App.js
+++ frontend/catalogo/src/App.js
@@ -5,6 +5,8 @@ import AdminServicios from "./pages/adminServicios";
 import Login from "./pages/login";
 import DetalleServicio from "./pages/detalleServicio";
 import SolicitarServicio from './pages/SolicitarServicio';
+import AdminRoles from "./pages/adminRoles";
+import AdminCategorias from "./pages/adminCategorias";
 import "./App.css";
 import Accesibilidad from "./components/accesibilidad";
 import Header from "./components/header";
@@ -23,6 +25,8 @@ function App() {
         <Route path="/servicio/:id" element={<DetalleServicio />} />
         <Route path="/solicitar" element={<SolicitarServicio />} />
         <Route path="/solicitar/:id" element={<SolicitarServicio />} />
+        <Route path="/admin/roles" element={<AdminRoles />} />
+        <Route path="/admin/categorias" element={<AdminCategorias />} />
 
       </Routes>
     </Router>
